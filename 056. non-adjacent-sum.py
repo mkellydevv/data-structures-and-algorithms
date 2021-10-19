9319a970668262ef89cc9ea6222379a410e9bb05
mk <m.kelly.devv@gmail.com>
2021-10-19 00:06:28 -0400

    Completed problem 057. summing-squares.py

--- 056. non-adjacent-sum.py
+++ 056. non-adjacent-sum.py
@@ -2,9 +2,12 @@ def non_adjacent_sum(nums):
   if len(nums) < 2:
     return max(nums)
 
-  sums = [nums[0], max(nums[0], nums[1])]
+  sum1 = nums[0]
+  sum2 = max(nums[0], nums[1])
 
   for i in range(2,len(nums)):
-    sums.append(max(sums[i-2] + nums[i], sums[i-1]))
+    tmp = sum2
+    sum2 = max(sum1 + nums[i], sum2)
+    sum1 = tmp
 
-  return sums[len(nums) - 1]
+  return sum2
