04c4b7bfd1a8a49f98ef8455ff68630dc72fdd7b
mk <m.kelly.devv@gmail.com>
2022-01-11 23:55:22 -0500

    Finished 004. is prime.

---
+++ javascript/004. is prime.js
@@ -0,0 +1,15 @@
+const isPrime = (n) => {
+    if (n < 2)
+        return false;
+
+    for (let i = Math.floor(Math.sqrt(n)); i > 1; i--) {
+        if (n % i === 0)
+            return false;
+    }
+
+    return true;
+};
+
+module.exports = {
+    isPrime
+};
