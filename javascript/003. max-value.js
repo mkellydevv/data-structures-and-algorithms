af40a96b3f426dc6e91306a01fe4000e202728d9
mk <m.kelly.devv@gmail.com>
2022-01-11 23:43:30 -0500

    Finished 003. max-value.

---
+++ javascript/003. max-value.js
@@ -0,0 +1,11 @@
+const maxValue = (nums) => {
+    let max = -Infinity;
+    for (const num of nums) {
+        max = Math.max(num, max);
+    }
+    return max;
+};
+
+module.exports = {
+    maxValue
+};
