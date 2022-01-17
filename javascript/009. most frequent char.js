fa387a9e54eb5892ebced44e69a110524e3f992c
mk <m.kelly.devv@gmail.com>
2022-01-16 23:30:54 -0500

    Finished 009. most frequent char.

---
+++ javascript/009. most frequent char.js
@@ -0,0 +1,25 @@
+const mostFrequentChar = (s) => {
+    const map = {};
+    let freq = null;
+    map[freq] = [-1,-1];
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] in map)
+            map[s[i]][0]++;
+        else
+            map[s[i]] = [1,i];
+    }
+
+    for (let key in map) {
+        if (map[key][0] > map[freq][0])
+            freq = key;
+        else if (map[key][0] === map[freq][0])
+            freq = map[key][1] < map[freq][1] ? key : freq;
+    }
+
+    return freq;
+};
+
+module.exports = {
+    mostFrequentChar
+};
