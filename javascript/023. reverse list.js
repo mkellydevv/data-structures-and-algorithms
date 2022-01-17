29e08e22ac6ded3678cacd8beb7804d7c88b8b68
mk <m.kelly.devv@gmail.com>
2022-01-17 17:30:44 -0500

    023. reverse list.js

---
+++ javascript/023. reverse list.js
@@ -0,0 +1,25 @@
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.next = null;
+//   }
+// }
+
+const reverseList = (head) => {
+  let prev = null;
+  let curr = head;
+
+  while (curr) {
+    const next = curr.next;
+    curr.next = prev;
+
+    prev = curr;
+    curr = next;
+  }
+
+  return prev;
+};
+
+module.exports = {
+  reverseList,
+};
