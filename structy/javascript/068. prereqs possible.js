12d506dbfce6c333c6410d00596e208110d1fb35
mk <m.kelly.devv@gmail.com>
2022-01-23 18:04:34 -0500

    Finished 068. prereqs possible.js

---
+++ structy/javascript/068. prereqs possible.js
@@ -0,0 +1,50 @@
+const prereqsPossible = (numCourses, prereqs) => {
+  const graph = buildGraph(numCourses, prereqs);
+
+  return !hasCycle(graph);
+};
+
+const hasCycle = (graph) => {
+  const visited = new Set();
+
+  for (let startNode in graph) {
+    if (detectCycle(graph, startNode, visited)) return true;
+  }
+
+  return false;
+};
+
+const detectCycle = (graph, node, visited, visiting = new Set()) => {
+  if (visited.has(node)) return false;
+  if (visiting.has(node)) return true;
+
+  visiting.add(node);
+
+  for (let neighbor of graph[node]) {
+    if (detectCycle(graph, neighbor, visited, visiting)) return true;
+  }
+
+  visiting.delete(node);
+  visited.add(node);
+
+  return false;
+};
+
+const buildGraph = (numCourses, prereqs) => {
+  const graph = {};
+
+  for (let i = 0; i < numCourses; i++) {
+    graph[i] = [];
+  }
+
+  for (let prereq of prereqs) {
+    const [a, b] = prereq;
+    graph[a].push(String(b));
+  }
+
+  return graph;
+};
+
+module.exports = {
+  prereqsPossible,
+};
