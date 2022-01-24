fe8f6016fa2ca98720c24a3a864d828ad9be3936
mk <m.kelly.devv@gmail.com>
2022-01-24 14:57:05 -0500

    Finished 089. paired parentheses.js

---
+++ structy/javascript/089. paired parentheses.js
@@ -0,0 +1,17 @@
+const pairedParentheses = (str) => {
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === '(') count++;
+    else if (str[i] === ')') {
+      count--;
+      if (count < 0) return false;
+    }
+  }
+
+  return count === 0;
+};
+
+module.exports = {
+  pairedParentheses,
+};
